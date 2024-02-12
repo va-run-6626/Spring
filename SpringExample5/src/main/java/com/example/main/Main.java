6b13bef375e74a5542a2a72bc60a9b65bbf6eba4
va-run-6626 <vbhutada143@gmail.com>
2024-02-12 17:56:46 +0530

    @Component example

---
+++ SpringExample5/src/main/java/com/example/main/Main.java
@@ -0,0 +1,19 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConifig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConifig.class);
+
+
+        var veh1 = context.getBean(Vehicle.class);
+        System.out.println("Vehicle object from Spring Context : "+ veh1.getName());
+        veh1.printHello();
+        System.out.println();
+
+    }
+}
